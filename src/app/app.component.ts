b75fd8d777866de7dc8cbb797b62eb684dfb3756
juan.salcedo <jsalcedo@proteccion.com.co>
2022-02-23 16:36:02 -0500

    El padre llama a un @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {//respetando el cilo de vida de angular para el renderizado
+
+
+  @ViewChild("identificadorH1") private miId!: ElementRef; //captturamos el elemneto h1 del html
+  @ViewChild("myInput") public input!: ElementRef;
+
   title = 'viewchild';
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.miId.nativeElement.innerText = "Se va cambiar el mensaje";
+    }, 2000);
+
+    this.input.nativeElement.focus();
+
+  }
+
+  addMensaje(){
+    alert("..."+this.input.nativeElement.value);
+  }
 }
